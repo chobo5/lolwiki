3fa84fc9e3d0780b4f25d105d3aed3c2d2ecc904
chobo5 <wnsdus1008@gmail.com>
2024-03-02 23:15:37 +0900

    .

--- lolwiki/app-server/src/main/java/org/example/dao/RelationshipDao.java
+++
@@ -1,8 +0,0 @@
-package org.example.dao;
-
-public interface RelationshipDao<T> {
-    void add(T t);
-    T findByForeignKey(int foreignKeyId); // 예시 메서드
-    int delete(int id, int foreignKeyId); // 예시 메서드
-    int update(T t);
-}
--- lolwiki/doc/query.sql
+++
@@ -1,7 +0,0 @@
-    select c.name,p.game_id,p.kor_name,p.kor_server_id,p.position
-        from player p,roaster r,season s,club c
-        where p.roaster_no=r.roaster_no
-        and r.season_no=2024
-        and s.season_no=2024
-        and s.club_no=c.club_no
-        order by c.name desc;
